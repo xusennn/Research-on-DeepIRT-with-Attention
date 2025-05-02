0fa56dc5bec9fd62e4b19606da8fa73a95d18bcf
xusennn <135446154+xusennn@users.noreply.github.com>
2025-05-02 21:32:04 +0800

    Update model_skill.py

--- model_skill.py
+++ model_skill.py
@@ -5,7 +5,7 @@ import tf_slim as slim
 from tf_slim import layers
 from memory_skill import DKVMN
 from utils import getLogger
-# Code reused from https://github.com/ckyeungac/DeepIRT.git
+
 
 # set logger
 logger = getLogger('Deep-IRT-model-HN')
